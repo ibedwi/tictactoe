a3b0aa4c1e4cd906b7d933647a2cdebbbc2403e2
Ibe Dwi <dwisatriakusuma4@gmail.com>
2022-11-13 12:48:46 +0700

    feat: init UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic Tac Toe</h1>
+
+      <div style={{ paddingBottom: 25}}>
+        <div>Player 1(X): [score placeholder]</div>
+        <div>Tie: [score placeholder]</div>
+        <div>Player 2(O): [score placeholder]</div>
+      </div>
+
+      <div className="grid-container">
+        {[1,2,3,4,5,6,7,8,9].map((n) => <div className="grid-item">{n}</div>)}
+      </div>
     </div>
   );
 }
